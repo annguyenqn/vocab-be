15b37bb4ac38297db3cf3a572af3c337a4e79c5c
AnNguyen <theesan2k1@gmail.com>
2024-09-11 14:05:54 +0700

    fix : seed role for user

--- src/database/seeds/seeder.ts
+++ src/database/seeds/seeder.ts
@@ -11,28 +11,19 @@ export class MainSeeder implements Seeder {
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
     const userFactory = factoryManager.get(User);
+    const userRepository = dataSource.getRepository(User);
     const roleRepository = dataSource.getRepository(Role);
-
     const adminRole = await roleRepository.save(
       roleRepository.create({ roleName: RoleName.ADMIN }),
     );
-    const userRole = await roleRepository.save(
-      roleRepository.create({ roleName: RoleName.USER }),
-    );
-
     const adminUser = new User();
     adminUser.email = 'admin@gmail.com';
     adminUser.firstName = 'Admin';
     adminUser.lastName = 'User';
     adminUser.password = await bcrypt.hash('admin', 10);
     adminUser.roles = [adminRole];
-    await dataSource.getRepository(User).save(adminUser);
-
     const users = await userFactory.saveMany(30);
-
-    for (const user of users) {
-      user.roles = [userRole];
-      await dataSource.getRepository(User).save(user);
-    }
+    await userRepository.save(users);
+    await userRepository.save(adminUser);
   }
 }
